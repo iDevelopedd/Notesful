1b770bc94ef48a3edffcf1d85f2dc5533f17d23c
iDevelopedd <fresdev12@gamil.com>
2023-02-17 12:19:42 -0800

    Modified some files and changed directory

--- Documents/repos/Notesful/src/Notesful/ui/SignupForm.java
+++ Documents/repos/Notesful/src/Notesful/ui/SignupForm.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package Notesful.ui;
 
 import Notesful.User;
 
-/**
- *
- * @author Prise
- */
 public class SignupForm extends javax.swing.JFrame {
 
     /**
